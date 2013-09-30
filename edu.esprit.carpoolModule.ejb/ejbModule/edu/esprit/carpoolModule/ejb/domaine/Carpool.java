dc3f0dcec39ac8c3cae0804f4c478e104fb7374d
Jawhar Sidhom <jawhar.sidhom@esprit.tn>
2013-09-30 21:16:41 +0100

    jawhar commit : new class abstraite Carpool

---
+++ edu.esprit.carpoolModule.ejb/ejbModule/edu/esprit/carpoolModule/ejb/domaine/Carpool.java
@@ -0,0 +1,14 @@
+package edu.esprit.carpoolModule.ejb.domaine;
+
+import java.io.Serializable;
+
+public abstract class Carpool implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+}
